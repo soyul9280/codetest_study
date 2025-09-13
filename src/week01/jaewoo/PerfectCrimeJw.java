cc4d81d8bd0e1e41c2bd5773e63d0f45cda393b5
백재우 <9712jw@gmail.com>
2025-09-13 13:43:03 +0900

    비용 배열을 정렬해서 최소값 구하기 - 오답

--- src/week01/jaewoo/PerfectCrimeJw.java
+++ src/week01/jaewoo/PerfectCrimeJw.java
@@ -2,6 +2,16 @@ class Solution {
 public int solution(int[][] info, int n, int m) {
         int ATotal = 0;
         int BTotal = 0;
+        
+        Arrays.sort(info, (costA, costB) -> {
+          // B 배열 수의 차
+          int sub = costB[1] - costA[1];
+          if (sub == 0) {
+              return costA[0] - costB[0];
+          }
+          return sub;
+        });
+        
         for (int[] leftOver : info) {
             int thiefA = leftOver[0];
             int thiefB = leftOver[1];
